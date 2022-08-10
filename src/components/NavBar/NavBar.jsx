1fc368d35f406ab21a69d4106ae658aec38448b5
cor2ba <cordobagabrielignacio@gmail.com>
2022-08-10 03:00:08 -0300

    Menu responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,45 +1,91 @@
 import React from "react";
 import Styles from "./NavBar.module.css";
 import { Link } from "react-router-dom";
+import menuimg from "../../images/hamburgermenu.png";
 
 const NavBar = ({ color }) => {
   return (
-    <div className={Styles.Container}>
-      <Link className={Styles.Link} to="/">
-        {color === "Home" ? (
-          <h1 className={Styles.NavsTextClicked}>HOME</h1>
-        ) : (
-          <h1 className={Styles.NavsText}>HOME</h1>
-        )}
-      </Link>
-      <Link className={Styles.Link} to="/LanguagesAndTools">
-        {color === "LanguagesAndTools" ? (
-          <h1 className={Styles.NavsTextClicked}>LANGUAGES AND TOOLS</h1>
-        ) : (
-          <h1 className={Styles.NavsText}>LANGUAGES AND TOOLS</h1>
-        )}
-      </Link>
-      <Link className={Styles.Link} to="/AboutMe">
-        {color === "AboutMe" ? (
-          <h1 className={Styles.NavsTextClicked}>ABOUT ME</h1>
-        ) : (
-          <h1 className={Styles.NavsText}>ABOUT ME</h1>
-        )}
-      </Link>
-      <Link className={Styles.Link} to="/Proyects">
-        {color === "Proyects" ? (
-          <h1 className={Styles.NavsTextClicked}>PROYECTS</h1>
-        ) : (
-          <h1 className={Styles.NavsText}>PROYECTS</h1>
-        )}
-      </Link>
-      <Link className={Styles.Link} to="/Contact">
-        {color === "Contact" ? (
-          <h1 className={Styles.NavsTextClicked}>CONTACT</h1>
-        ) : (
-          <h1 className={Styles.NavsText}>CONTACT</h1>
-        )}
-      </Link>
+    <div>
+      <div className={Styles.ContainerResponsive}>
+        <label for="menu" className={Styles.MenuImg}>
+          <img className={Styles.Img} src={menuimg} alt="Menu" />
+        </label>
+        <input type="checkbox" id="menu" className={Styles.NavInput} />
+        <div className={Styles.Menu}>
+          <Link className={Styles.Link} to="/">
+            {color === "Home" ? (
+              <h1 className={Styles.NavsTextClicked}>HOME</h1>
+            ) : (
+              <h1 className={Styles.NavsText}>HOME</h1>
+            )}
+          </Link>
+          <Link className={Styles.Link} to="/LanguagesAndTools">
+            {color === "LanguagesAndTools" ? (
+              <h1 className={Styles.NavsTextClicked}>LANGUAGES AND TOOLS</h1>
+            ) : (
+              <h1 className={Styles.NavsText}>LANGUAGES AND TOOLS</h1>
+            )}
+          </Link>
+          <Link className={Styles.Link} to="/AboutMe">
+            {color === "AboutMe" ? (
+              <h1 className={Styles.NavsTextClicked}>ABOUT ME</h1>
+            ) : (
+              <h1 className={Styles.NavsText}>ABOUT ME</h1>
+            )}
+          </Link>
+          <Link className={Styles.Link} to="/Proyects">
+            {color === "Proyects" ? (
+              <h1 className={Styles.NavsTextClicked}>PROYECTS</h1>
+            ) : (
+              <h1 className={Styles.NavsText}>PROYECTS</h1>
+            )}
+          </Link>
+          <Link className={Styles.Link} to="/Contact">
+            {color === "Contact" ? (
+              <h1 className={Styles.NavsTextClicked}>CONTACT</h1>
+            ) : (
+              <h1 className={Styles.NavsText}>CONTACT</h1>
+            )}
+          </Link>
+        </div>
+      </div>
+      <div className={Styles.Container}>
+        <Link className={Styles.Link} to="/">
+          {color === "Home" ? (
+            <h1 className={Styles.NavsTextClicked}>HOME</h1>
+          ) : (
+            <h1 className={Styles.NavsText}>HOME</h1>
+          )}
+        </Link>
+        <Link className={Styles.Link} to="/LanguagesAndTools">
+          {color === "LanguagesAndTools" ? (
+            <h1 className={Styles.NavsTextClicked}>LANGUAGES AND TOOLS</h1>
+          ) : (
+            <h1 className={Styles.NavsText}>LANGUAGES AND TOOLS</h1>
+          )}
+        </Link>
+        <Link className={Styles.Link} to="/AboutMe">
+          {color === "AboutMe" ? (
+            <h1 className={Styles.NavsTextClicked}>ABOUT ME</h1>
+          ) : (
+            <h1 className={Styles.NavsText}>ABOUT ME</h1>
+          )}
+        </Link>
+        <Link className={Styles.Link} to="/Proyects">
+          {color === "Proyects" ? (
+            <h1 className={Styles.NavsTextClicked}>PROYECTS</h1>
+          ) : (
+            <h1 className={Styles.NavsText}>PROYECTS</h1>
+          )}
+        </Link>
+        <Link className={Styles.Link} to="/Contact">
+          {color === "Contact" ? (
+            <h1 className={Styles.NavsTextClicked}>CONTACT</h1>
+          ) : (
+            <h1 className={Styles.NavsText}>CONTACT</h1>
+          )}
+        </Link>
+      </div>
     </div>
   );
 };
